8cec81bfe95f16277dba412e22e8274a795a177e
Oliver Hader <oliver@typo3.org>
2019-12-23 16:58:10 +0100

    [TASK] Streamline acceptance tests & upgrade to codeception 4.0

--- tests/Acceptance/FrontendPagesCest.php
+++ tests/Acceptance/FrontendPagesCest.php
@@ -1,4 +1,5 @@
-<?php namespace OliverHader\BookStoreProject\Tests\Acceptance\Support;
+<?php
+namespace OliverHader\BookStoreProject\Tests\Acceptance\Support;
 use OliverHader\BookStoreProject\Tests\Acceptance\Support\AcceptanceTester;
 
 class FrontendPagesCest
@@ -7,24 +8,32 @@ class FrontendPagesCest
     {
     }
 
-    // tests
-    public function areImportantLinksAvailableInNavigation(AcceptanceTester $I, AdminTest $Admin)
+    public function areImportantLinksAvailableInMainNavigation(AcceptanceTester $I)
     {
+        $I->wantToTest('Main navigation is correct');
         $I->amOnPage('/');
         $I->canSee('Company', 'nav#mainnavigation');
-        $I->cantSee('Logout', 'nav#mainnavigation');
+        $I->canSee('Legal Information', 'nav#mainnavigation');
     }
 
-    public function ICanLoginAndLogoutAgain(AcceptanceTester $I)
+    public function user001CanLoginAndLogoutAgain(AcceptanceTester $I)
     {
+        $I->wantToTest('Frontend loging and logout capabilities using user-001');
         $I->amOnPage('/');
         $I->canSee('My Book Store', 'nav#mainnavigation');
         $I->click('My Book Store', 'nav#mainnavigation');
-        $I->canSee('User login');
+
+        $I->canSee('User login', 'main');
+        $I->cantSee('Logout', 'nav#mainnavigation');
         $I->fillField('user', 'user-001');
         $I->fillField('pass', 'password');
-        $I->click('input[type=submit]');
-        $I->canSee('You are now logged in as \'user-001\'');
-        $I->makeScreenshot('logged-in');
+        $I->click('input[type=submit]', 'form');
+
+        $I->canSee('You are now logged in as \'user-001\'', 'main');
+        $I->canSee('Logout', 'nav#mainnavigation');
+        $I->makeScreenshot('test_user-001_logged-in');
+
+        $I->click('Logout', 'nav#mainnavigation');
+        $I->cantSee('Logout', 'nav#mainnavigation');
     }
 }
